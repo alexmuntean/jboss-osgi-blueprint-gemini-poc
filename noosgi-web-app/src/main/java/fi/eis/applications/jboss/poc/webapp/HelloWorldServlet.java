c7089e55eca99940d6c8ee7cfcbc5299c9036e63
eis <eis@iki.fi>
2012-12-10 14:29:06 +0200

    noosgi-web-app is able to access the bundle using servicelocator

--- noosgi-web-app/src/main/java/fi/eis/applications/jboss/poc/webapp/HelloWorldServlet.java
+++ noosgi-web-app/src/main/java/fi/eis/applications/jboss/poc/webapp/HelloWorldServlet.java
@@ -18,64 +18,111 @@ import javax.servlet.http.HttpServletResponse;
 import org.jboss.logging.Logger;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceReference;
 
 import fi.eis.applications.jboss.poc.osgiservice.api.MessageService;
+import org.osgi.util.tracker.ServiceTracker;
 
 /**
  * @url http://localhost:8080/jboss-poc-webapp/HelloWorld
  */
 @SuppressWarnings("serial")
 @WebServlet("/HelloWorld")
-public class HelloWorldServlet extends HttpServlet {
+public class HelloWorldServlet extends HttpServlet implements Servlet {
 
   private static Logger log = Logger.getLogger(HelloWorldServlet.class);
 
-  @Resource
   private MessageService service;
 
   @Resource
-  private BundleContext context;
+  private BundleContext bundleContext;
+  // another way of obtaining bundle context would be to run this on init method:
+  // bundleContext = (BundleContext) config.getServletContext().getAttribute("osgi-bundlecontext");
+  // but that wouldn't that nice as it introduces tight coupling
 
+  
   @Override
   public void init(final ServletConfig config) throws ServletException {
 	  super.init(config);
-	  log.debug("Initialized");
-  }
+      final HttpServlet servlet = this;
+      
+      
+      log.infof("Registering with parameters %s, %s, %s", bundleContext, MessageService.class.getName(), null);
+       
+      // TODO is this possible with just Blueprint XML configuration?
+      // @Autowired for the service does not work
+      ServiceTracker serviceTracker = new ServiceTracker(bundleContext, MessageService.class.getName(), null){
+           @Override
+           public Object addingService(final ServiceReference sref) {
+             log.infof("Adding service: %s to %s", sref, servlet);
+             service = (MessageService) super.addingService(sref);
+             return service;
+           }
 
-  public void setMessageService(MessageService service) {
-	  log.debug("Setting service: " + service);
-	  this.service = service;
+           @Override
+           public void removedService(final ServiceReference sref, final Object sinst) {
+             super.removedService(sref, service);
+             log.infof("Removing service: %s from %s", sref, servlet);
+             service = null;
+           }    	  
+       };  
+       serviceTracker.open(); 	  
+	  log.info("Initialized");
   }
-  public MessageService getMessageService() {
-	  return this.service;
-  }  
-  
+ 
   private static String PAGE_HEADER = "<html><head><title>helloworld</title><body>";
   private static String PAGE_FOOTER = "</body></html>";
 
   @Override
   protected void doGet(final HttpServletRequest req,
       final HttpServletResponse resp) throws ServletException, IOException {
-	List<String> bundles = new ArrayList<String>();
-	for (Bundle bundle : context.getBundles()) {
-		bundles.add(bundle.getSymbolicName() + ":" + bundle.getVersion().toString());
-	}
-	Collections.sort(bundles);
-	  
     resp.setContentType("text/html");
     PrintWriter writer = resp.getWriter();
     writer.println(PAGE_HEADER);
     writer.println("<h1>" + (service != null ? service.getMessage() : "null") + "</h1>");
-    writer.println("Using BundleContext looked up in Servlet Context to list available OSGi Bundles:");
-	writer.println("<ul>");
-    for (String bundleInfo : bundles) {
-    	writer.print("<li>");
-        writer.print(bundleInfo);
-    	writer.println("</li>");
+    if (bundleContext == null)
+    {
+	    writer.print("BundleContext null, cannot use BundleContext to list available OSGi Bundles");
+    } else
+    {
+	    writer.print("Using BundleContext looked up in Servlet Context to list available OSGi Bundles:");
+	    listBundlesWith(bundleContext, writer);
     }
-	writer.println("</ul>");
     writer.println(PAGE_FOOTER);
     writer.close();
   }
+  private static void listBundlesWith(BundleContext context, PrintWriter writer) {
+		List<String> bundles = new ArrayList<String>();
+		for (Bundle bundle : context.getBundles()) {
+			bundles.add(bundle.getSymbolicName() + ":" + bundle.getVersion().toString());
+		}
+		Collections.sort(bundles);
+		List<String> services = new ArrayList<String>();
+		try {
+			for (ServiceReference ref : context.getAllServiceReferences(null, null)) {
+				services.add(context.getService(ref).toString());
+				context.ungetService(ref);
+			}
+		} catch (InvalidSyntaxException e) {
+			throw new IllegalStateException(e);
+		}
+		Collections.sort(services);
+		writer.println("<ul>");
+	    for (String bundleInfo : bundles) {
+	    	writer.print("<li>");
+	        writer.print(bundleInfo);
+	    	writer.println("</li>");
+	    }
+		writer.println("</ul>");
+		writer.println("<ul>");
+	    for (String service : services) {
+	    	writer.print("<li>");
+	        writer.print(service);
+	    	writer.println("</li>");
+	    }
+		writer.println("</ul>");
+		
+  }  
 
 }
