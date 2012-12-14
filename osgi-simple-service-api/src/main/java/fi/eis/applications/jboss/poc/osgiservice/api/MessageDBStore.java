48d87501a6ce1eff1a20750a67df5d8edf063b6f
eis <eis@iki.fi>
2012-12-14 15:00:53 +0200

    no need for xa/sql dependencies at api level

--- osgi-simple-service-api/src/main/java/fi/eis/applications/jboss/poc/osgiservice/api/MessageDBStore.java
+++ osgi-simple-service-api/src/main/java/fi/eis/applications/jboss/poc/osgiservice/api/MessageDBStore.java
@@ -1,10 +1,6 @@
 package fi.eis.applications.jboss.poc.osgiservice.api;
 
-import javax.sql.XAConnection;
-
 public interface MessageDBStore {
-	public XAConnection getXAConnection(); 
 	public Long persistMessage(String message);
-	public Long persistMessageXA(XAConnection conn, String message);
 	public String getMessage(Long id);
 }
